03fc8be178906682c1210d7fa0560ea66aac1144
Admin - Dubey <ndubey-adm@amgen.com>
2019-01-18 08:28:51 -0800

    updated

--- src/main/java/com/in28minutes/rest/webservices/restfulweb/service/Application.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulweb/service/Application.java
@@ -11,14 +11,7 @@ public class Application {
 		System.out.println("Hi naveen"); 
 		System.out.println("Hi Test");
 		System.out.println("Hi Test 2 ");
-		
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3")
-		
-		System.out.println("test");
-		System.out.println("Test Nveeeeeeeeeeeeeeeeeeeee");
-		System.out.println("Test Nveeeeeeeeeeeeeeeeeeeee");
+		System.out.println("Naresh");
 		
 		
 		
