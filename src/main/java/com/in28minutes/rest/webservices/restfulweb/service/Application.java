47a1b3bf842ced3984988dd953fb786b7d24fa5a
Admin - Dubey <ndubey-adm@amgen.com>
2019-01-18 01:50:03 -0800

    add

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulweb/service/Application.java
@@ -0,0 +1,26 @@
+package com.in28minutes.rest.webservices.restfulweb.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Hi naveen"); 
+		System.out.println("Hi Test");
+		System.out.println("Hi Test 2 ");
+		System.out.println("test");
+		System.out.println("Test Nveeeeeeeeeeeeeeeeeeeee");
+		System.out.println("Test Nveeeeeeeeeeeeeeeeeeeee");
+		
+		//Added by Naveen 
+		
+		SpringApplication.run(Application.class, args);
+		
+		
+	}
+
+}
+
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulweb/service/Test.java
@@ -0,0 +1,5 @@
+package com.in28minutes.rest.webservices.restfulweb.service;
+
+public class Test {
+
+}
