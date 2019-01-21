defcc245f0b6b89fd3c5842bdec3d91cebc58dae
Admin - Dubey <ndubey-adm@amgen.com>
2019-01-21 01:11:00 -0800

    add

--- src/main/java/com/in28minutes/rest/webservices/restfulweb/service/Application.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulweb/service/Application.java
@@ -11,7 +11,7 @@ public class Application {
 		System.out.println("Hi naveen"); 
 		System.out.println("Hi Test");
 		System.out.println("Hi Test 2 ");
-         System.out.println("Naresh");
+        
 
 		
 
